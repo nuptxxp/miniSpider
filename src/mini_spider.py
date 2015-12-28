675fd10d9f77c002397c819b19f2695143873b24
nuptxxp <pxiangxiong@gmail.com>
2015-12-28 19:28:34 +0800

    add src

---
+++ src/mini_spider.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# author : pengxiangxiong
+# email  : pengxiangxiong@baidu.com
+
+import sys
+sys.path.append('src')
+
+class MiniSpider(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+
+
+# vim: set expandtab ts=4 sw=4 sts=4 tw=100:
+
